aa71c3ddb0ffbc1ff8ccd325e5ec1ea13ab7be77
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-08-14 09:14:06 -1000

    Build basic page routing

---
+++ src/pages/PostPage.jsx
@@ -0,0 +1,15 @@
+function PostPage() {
+    return (
+        <>
+            <div>
+                <h1>Post Title</h1>
+            </div>
+            <div>
+                <h2>Post comments</h2>
+            </div>
+        </>
+        
+    )
+}
+
+export default PostPage
