3edc7b558f7fe1f1b2aae70cbc79e84cef6f343b
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-08-14 15:16:44 -1000

    Create page for individual posts

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,8 +1,55 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
 function PostPage() {
+    const { postId } = useParams();
+
+    const [post, setPost] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchPost = async () => {                
+            try {
+                const response = await fetch(`http://localhost:3000/api/posts/${postId}`);
+                console.log('Got response', response)
+                
+                if (response.status === 404) {
+                    setError('Post not found')
+                    setIsLoading(false)
+                    return
+                }
+                if (!response.ok) {
+                    throw new Error('Failed to fetch post')
+                }
+
+                const data = await response.json();
+                setPost(data);
+            }
+            catch (err) {
+                setError(err.message)
+            } finally {
+                setIsLoading(false);
+            }
+        }  
+
+        fetchPost()
+    }, [postId])
+
+    if (isLoading) {
+        return (<h1>Loading...</h1>)
+    }
+
+    if (error) {
+        console.log(error)
+        return (<h1>{error}</h1>)
+    }
+
     return (
         <>
             <div>
-                <h1>Post Title</h1>
+                <h1>{post.title}</h1>
+                <p>{post.text}</p>
             </div>
             <div>
                 <h2>Post comments</h2>
